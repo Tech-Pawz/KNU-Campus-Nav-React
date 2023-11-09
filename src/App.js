0aa0e796c50c5fdd98a291e3858a4c9d7b7e3096
ny030303 <51257615+ny030303@users.noreply.github.com>
2023-11-09 19:24:19 +0900

    add chatbot UI

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 function App() {
   const [location, setLoacation] = useState(null); // 현재 위치를 저장할 상태
-
+  const [position, setPosition] = useState();
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(successHandler, errorHandler); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
 	}, []);
@@ -30,15 +30,18 @@ function App() {
       <div className='map-wrap'>
         {location && (
           <Map 
-              center={{ lat: location.latitude, lng: location.longitude }} 
+              center={{lat: "37.2771248", lng: "127.1341975" }} 
               style={{ width: '100%', height: '100%' }} 
               level={3}
             >
-            <MapMarker position={{ lat: location.latitude, lng: location.longitude }} />
+            <MapMarker position={{lat: "37.2771248", lng: "127.1341975" }} />
           </Map>
         )}
       </div>
-      
+      <div id='chatbot-wrap'>
+        <img src='./images/icons8-chatbot-96.png' alt='icon' id='chatbot'/>
+      </div>
+     
     </div>
   );
 }
